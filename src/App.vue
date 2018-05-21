e333b1e56767b959f5ae452ec82e16c72fc02308
gss.es-tkkim <es-tkkim@smilegate.com>
2018-05-21 18:41:56 +0900

    babel eslint add

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+      v-model="sideNav"
+      temporary
+      fixed
+      app>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon></v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar>
+      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav"></v-toolbar-side-icon>
+      <v-toolbar-title>DevMeetUp</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left>supervisor_account</v-icon>
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+
+    </main>
   </v-app>
 </template>
 
@@ -7,6 +34,7 @@
 export default {
   data () {
     return {
+      sideNav: false
     }
   },
   name: 'App'
